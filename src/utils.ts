ee18e9c79218bdbfa43aab733d8526552b1321e8
weaponsforge <forgeweapons@gmail.com>
2025-02-12 02:46:28 +0800

    chore: install/setup prettier

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
 type MathFunction = (a: number, b: number) => number;
 
-export const addition: MathFunction = (a, b) => a + b
+export const addition: MathFunction = (a, b) => a + b;
 
-export const subtraction: MathFunction = (a, b) => a - b
+export const subtraction: MathFunction = (a, b) => a - b;
 
 export const multiplication: MathFunction = (a, b) => a * b;
 
